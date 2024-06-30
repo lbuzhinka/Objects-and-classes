abe2cab2126334e60a704f46e4a57a13428f9f2b
lbuzhinka <lbuzhinka@gmail.com>
2024-06-30 16:32:08 +0300

    домашка

--- src/Book.java
+++ src/Book.java
@@ -1,13 +1,20 @@
 public class Book {
    private String bookName;
+   static Author author;
    private int yearOfPublication;
-   public Book (String bookName, int yearOfPublication) {
+   private Book(String bookName, Author author, int yearOfPublication) {
       this.bookName = bookName;
+      this.author = author;
       this.yearOfPublication = yearOfPublication;
    }
-   public String getBookName () {
+
+    public static Book createBook(String bookName,  int yearOfPublication) {
+        return new Book(bookName, author, yearOfPublication);
+    }
+    public String getBookName () {
       return this.bookName;
    }
+
    public int getYearOfPublication () {
       return this.yearOfPublication;
    }
